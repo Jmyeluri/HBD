4821d3453e390d64be47f9c5372716a9368a85f9
Jmyeluri <jyelur2@illinois.edu>
2016-06-20 14:20:52 -0500

    first

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	//$('#button').click()
+	  $('.materialboxed').materialbox();
+   $('.carousel').carousel();
+   $('.carousel').hide();
+   $('#button').click(function(){
+   		$('.carousel').show();
+   		$('#button').addClass()
+
+   });
+   
+
+
+});
